b93006a89259fd2250966a1b97181e483f3b6399
nadymahmoud <asmaylsmt70@gmail.com>
2024-03-17 15:11:11 +0200

    Add new file app_colors.dart

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static Color darkPurple = const Color(0xff0F0731);
+  static Color lightwhite = const Color(0xffEBE9FE);
+  static Color gradientOne = const Color(0xff00FFE0);
+
+  static Color gradientTwo = const Color(0xffED00C7);
+
+  static Color darkgrey = const Color(0xff3E2F65);
+}
