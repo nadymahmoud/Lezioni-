900d2faa6cf8690c0066d19f2eacaf31e820aedc
nadymahmoud <asmaylsmt70@gmail.com>
2024-03-17 15:11:34 +0200

    Add assets class with image paths
    Added a new class `Assets` with static constants for various image paths in the project. This will help in managing and accessing image assets easily throughout the application.

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,24 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesBackgroundImage
+  /// assests/images/backgroundImage.png
+  static const String imagesBackgroundImage = "assests/images/backgroundImage.png";
+
+  /// Assets for imagesHomeActive
+  /// assests/images/HomeActive.svg
+  static const String imagesHomeActive = "assests/images/HomeActive.svg";
+
+  /// Assets for imagesIcon
+  /// assests/images/icon.svg
+  static const String imagesIcon = "assests/images/icon.svg";
+
+  /// Assets for imagesRose
+  /// assests/images/rose.svg
+  static const String imagesRose = "assests/images/rose.svg";
+
+  /// Assets for imagesVector
+  /// assests/images/Vector.svg
+  static const String imagesVector = "assests/images/Vector.svg";
+}
+
