1ffb478fe6d9bfc78159ec155bd60356cf02d9d9
nadymahmoud <asmaylsmt70@gmail.com>
2024-03-17 14:08:26 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/lezioni/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lezioni
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const LezioniApp());
+}
+
+class LezioniApp extends StatelessWidget {
+  const LezioniApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
