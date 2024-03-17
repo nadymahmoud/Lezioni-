e6f73e655687e1c3ac633c631a3353f8992dab99
nadymahmoud <asmaylsmt70@gmail.com>
2024-03-17 15:41:41 +0200

    Add initial version of AppStrings class
    This commit adds the initial version of the AppStrings class with authentication
    related strings defined. The class includes strings for creating an account,
    name, email address, password, and sign in functionality.

---
+++ lib/core/utils/app_strings.dart
@@ -0,0 +1,14 @@
+abstract class AppStrings {
+  //ToDo:All Strings
+//!auth Strings
+//!Create Your Account
+  static const String createYourAccount = "Create Your Account",
+      name = "Name",
+      fullName = "Full Name",
+      emailAddress = 'Email address',
+      exampleEmail = 'you@example.com',
+      password = 'Password',
+      examplePassword = '********',
+      alreadyHaveAnAccount = "Already have an account? ",
+      signIn = 'Sign in';
+}
