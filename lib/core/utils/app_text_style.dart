2563fe9090fa03ef0163c3b1a29a226d1976d31d
nadymahmoud <asmaylsmt70@gmail.com>
2024-03-17 16:06:06 +0200

    Add Inter-Regular font to pubspec.yaml
    Add Inter-Regular font to the pubspec.yaml file under the fonts section. This font will be used for text styling in the application.

---
+++ lib/core/utils/app_text_style.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:lezioni/core/utils/app_colors.dart';
+
+abstract class CustomTextStyle {
+  static final inter600Style20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+    color: AppColors.lightwhite,
+    fontFamily: 'Inter',
+  );
+}
