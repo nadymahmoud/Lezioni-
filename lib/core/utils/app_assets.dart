d178fc10a493869b67fbcaffb91bb25b6789eab4
nadymahmoud <asmaylsmt70@gmail.com>
2024-03-24 19:29:09 +0200

    Add new image assets and update existing ones
    Added new image assets and updated existing ones in the Assets class.
    This commit improves the organization and accessibility of image assets
    within the application.

--- lib/core/utils/app_assets.dart
+++ lib/core/utils/app_assets.dart
@@ -1,32 +1,135 @@
 class Assets {
   Assets._();
-  
+
+  /// Assets for images15
+  /// assests/images/15.svg
+  static const String images15 = "assests/images/15.svg";
+
   /// Assets for imagesBackgroundImage
   /// assests/images/backgroundImage.png
-  static const String imagesBackgroundImage = "assests/images/backgroundImage.png";
+  static const String imagesBackgroundImage =
+      "assests/images/backgroundImage.png";
+
+  /// Assets for imagesBackgroundImagesvg
+  /// assests/images/backgroundImagesvg.svg
+  static const String imagesBackgroundImagesvg =
+      "assests/images/backgroundImagesvg.svg";
+
+  /// Assets for imagesBottomIcon
+  /// assests/images/bottom_icon.svg
+  static const String imagesBottomIcon = "assests/images/bottom_icon.svg";
+
+  /// Assets for imagesCommunityActive
+  /// assests/images/communityActive.svg
+  static const String imagesCommunityActive =
+      "assests/images/communityActive.svg";
+
+  /// Assets for imagesCommunityIcons
+  /// assests/images/community_icons.svg
+  static const String imagesCommunityIcons =
+      "assests/images/community_icons.svg";
+
+  /// Assets for imagesDarkIcons
+  /// assests/images/dark_icons.svg
+  static const String imagesDarkIcons = "assests/images/dark_icons.svg";
+
+  /// Assets for imagesDegreeActive
+  /// assests/images/degreeActive.svg
+  static const String imagesDegreeActive = "assests/images/degreeActive.svg";
+
+  /// Assets for imagesDegreeIcons
+  /// assests/images/degree_icons.svg
+  static const String imagesDegreeIcons = "assests/images/degree_icons.svg";
+
+  /// Assets for imagesDuotone
+  /// assests/images/Duotone.svg
+  static const String imagesDuotone = "assests/images/Duotone.svg";
+
+  /// Assets for imagesExistIcon
+  /// assests/images/exist_icon.svg
+  static const String imagesExistIcon = "assests/images/exist_icon.svg";
 
   /// Assets for imagesHomeActive
   /// assests/images/HomeActive.svg
   static const String imagesHomeActive = "assests/images/HomeActive.svg";
 
+  /// Assets for imagesHomeIcons
+  /// assests/images/home_icons.svg
+  static const String imagesHomeIcons = "assests/images/home_icons.svg";
+
   /// Assets for imagesIcon
   /// assests/images/icon.svg
   static const String imagesIcon = "assests/images/icon.svg";
+
+  /// Assets for imagesIcon3
+  /// assests/images/icon3.svg
+  static const String imagesIcon3 = "assests/images/icon3.svg";
+
+  /// Assets for imagesLightIcon
+  /// assests/images/light_icon.svg
+  static const String imagesLightIcon = "assests/images/light_icon.svg";
+
+  /// Assets for imagesLogoutIcons
+  /// assests/images/logout_icons.svg
+  static const String imagesLogoutIcons = "assests/images/logout_icons.svg";
 
   /// Assets for imagesMaskGroup
   /// assests/images/Mask_group.png
   static const String imagesMaskGroup = "assests/images/Mask_group.png";
 
+  /// Assets for imagesMaxIcons
+  /// assests/images/max_icons.svg
+  static const String imagesMaxIcons = "assests/images/max_icons.svg";
+
+  /// Assets for imagesMusicActive
+  /// assests/images/musicActive.svg
+  static const String imagesMusicActive = "assests/images/musicActive.svg";
+
+  /// Assets for imagesMusicIcons
+  /// assests/images/music_icons.svg
+  static const String imagesMusicIcons = "assests/images/music_icons.svg";
+
+  /// Assets for imagesNotifications
+  /// assests/images/notifications.svg
+  static const String imagesNotifications = "assests/images/notifications.svg";
+
+  /// Assets for imagesProfile
+  /// assests/images/Profile.svg
+  static const String imagesProfile = "assests/images/Profile.svg";
+
+  /// Assets for imagesProfilePng
+  /// assests/images/ProfilePng.png
+  static const String imagesProfilePng = "assests/images/ProfilePng.png";
+
   /// Assets for imagesRose
   /// assests/images/rose.svg
   static const String imagesRose = "assests/images/rose.svg";
+
+  /// Assets for imagesRoseDrawer
+  /// assests/images/rose_drawer.svg
+  static const String imagesRoseDrawer = "assests/images/rose_drawer.svg";
+
+  /// Assets for imagesSearch
+  /// assests/images/search.svg
+  static const String imagesSearch = "assests/images/search.svg";
 
   /// Assets for imagesShowPassword
   /// assests/images/showPassword.svg
   static const String imagesShowPassword = "assests/images/showPassword.svg";
 
+  /// Assets for imagesSkyRight
+  /// assests/images/skyRight.svg
+  static const String imagesSkyRight = "assests/images/skyRight.svg";
+
+  /// Assets for imagesSkyRightpng
+  /// assests/images/skyRightpng.png
+  static const String imagesSkyRightpng = "assests/images/skyRightpng.png";
+
+  /// Assets for imagesSwitchIcons
+  /// assests/images/switch_icons.svg
+  static const String imagesSwitchIcons = "assests/images/switch_icons.svg";
+
   /// Assets for imagesVector
   /// assests/images/Vector.svg
   static const String imagesVector = "assests/images/Vector.svg";
 }
-
